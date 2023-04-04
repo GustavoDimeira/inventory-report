01b231aded08327c2eff5489b0cf0f3d3e2b0699
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-04 13:58:59 -0400

    tests: create new product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    newProduct = Product(
+        1,
+        'newProduct',
+        'newCompany',
+        '01/01/1001',
+        '01/01/2001',
+        '0915629628',
+        '',
+    )
+
+    assert newProduct.id == 1
+    assert newProduct.nome_do_produto == 'newProduct'
+    assert newProduct.nome_da_empresa == 'newCompany'
+    assert newProduct.data_de_fabricacao == '01/01/1001'
+    assert newProduct.data_de_validade == '01/01/2001'
+    assert newProduct.numero_de_serie == '0915629628'
+    assert newProduct.instrucoes_de_armazenamento == ''
