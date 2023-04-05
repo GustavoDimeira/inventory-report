4c18f1b917a75cd36b5590d18ac34e58252f8f27
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-04 20:31:11 -0400

    fix

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,45 +1,46 @@
 from datetime import datetime
 
 
+def get_older_X_spoiling():
+    older = False
+    spoiling = False
+    products_counter = {}
+
+    for iten in list:
+        (
+            year_f,
+            month_f,
+            day_f,
+        ) = iten["data_de_fabricacao"].split("-", 2)
+
+        (
+            year_v,
+            month_v,
+            day_v,
+        ) = iten["data_de_validade"].split("-", 2)
+
+        fabrication_date = datetime(int(year_f), int(month_f), int(day_f))
+        validation_date = datetime(int(year_v), int(month_v), int(day_v))
+
+        if (not older or fabrication_date < older):
+            older = fabrication_date
+
+        if (not spoiling or validation_date < spoiling):
+            spoiling = validation_date
+
+        if iten["nome_da_empresa"] in products_counter:
+            products_counter[iten["nome_da_empresa"]] += 1
+        else:
+            products_counter[iten["nome_da_empresa"]] = 1
+    return (older, spoiling, products_counter)
+
+
 class SimpleReport:
-    def get_older_X_spoiling():
-        older = False
-        spoiling = False
-        products_counter = {}
-
-        for iten in list:
-            (
-                year_f,
-                month_f,
-                day_f,
-            ) = iten["data_de_fabricacao"].split("-", 2)
-
-            (
-                year_v,
-                month_v,
-                day_v,
-            ) = iten["data_de_validade"].split("-", 2)
-
-            fabrication_date = datetime(int(year_f), int(month_f), int(day_f))
-            validation_date = datetime(int(year_v), int(month_v), int(day_v))
-
-            if (not older or fabrication_date < older):
-                older = fabrication_date
-
-            if (not spoiling or validation_date < spoiling):
-                spoiling = validation_date
-
-            if iten["nome_da_empresa"] in products_counter:
-                products_counter[iten["nome_da_empresa"]] += 1
-            else:
-                products_counter[iten["nome_da_empresa"]] = 1
-        return (older, spoiling, products_counter)
-
     @staticmethod
-    def generate(self, list: list):
+    def generate(list: list):
         most_products = list[0]["nome_da_empresa"]
 
-        (older, spoiling, products_counter) = self.get_older_X_spoiling()
+        (older, spoiling, products_counter) = get_older_X_spoiling()
 
         for company in products_counter:
             if (products_counter[company] > products_counter[most_products]):
