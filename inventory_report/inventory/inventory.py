368cbd7dd0894d478eed76abb54a4ebeb52d0013
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-17 16:41:36 -0400

    xml reader

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -8,13 +9,16 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory():
     @staticmethod
     def import_data(path: str, type: str):
-        if (path.endswith(".csv")):
-            file = csv.DictReader(open(path), delimiter=",", quotechar='"')
-            list = [line for line in file]
-        elif (path.endswith(".json")):
-            list = json.load(open(path))
-        elif (path.endswith(".XML")):
-            pass
+        list = []
+        with open(path) as fileRaw:
+            if (path.endswith(".csv")):
+                file = csv.DictReader(fileRaw, delimiter=",", quotechar='"')
+                list = [line for line in file]
+            elif (path.endswith(".json")):
+                list = json.load(fileRaw)
+            elif (path.endswith(".xml")):
+                file = fileRaw.read()
+                list = xmltodict.parse(file)['dataset']['record']
 
         if (type == "simples"):
             return SimpleReport.generate(list)
