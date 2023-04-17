33b709a4491843d3b99928f3541b116094526f4c
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-17 14:27:51 -0400

    feat: readding json files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -7,12 +8,14 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory():
     @staticmethod
     def import_data(path: str, type: str):
-        file = csv.DictReader(open(path), delimiter=",", quotechar='"')
-        list = [line for line in file]
+        if (path.endswith(".csv")):
+            file = csv.DictReader(open(path), delimiter=",", quotechar='"')
+            list = [line for line in file]
+        elif (path.endswith(".json")):
+            list = json.load(open(path))
+        elif (path.endswith(".XML")):
+            pass
 
         if (type == "simples"):
             return SimpleReport.generate(list)
-        elif (type == "completo"):
-            return CompleteReport.generate(list)
-        else:
-            print("Tipo incorreto")
+        return CompleteReport.generate(list)
