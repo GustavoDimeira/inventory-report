d700b16c8b58c6f066b008069cb893b0e8094daa
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-17 14:06:18 -0400

    feat: generat csv report

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,18 @@
+import csv
+
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
 class Inventory():
     @staticmethod
-    def import_data():
-        pass
+    def import_data(path: str, type: str):
+        file = csv.DictReader(open(path), delimiter=",", quotechar='"')
+        list = [line for line in file]
+
+        if (type == "simples"):
+            return SimpleReport.generate(list)
+        elif (type == "completo"):
+            return CompleteReport.generate(list)
+        else:
+            print("Tipo incorreto")
