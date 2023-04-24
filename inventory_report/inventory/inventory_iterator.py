fbf9c04c6859021952bc62e16166e53910faf563
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-24 13:52:06 -0400

    feat: iterating inventory class

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,13 @@
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    def __init__(self, data):
+        self.data = data
+        self.cicle = 0
+
+    def __next__(self):
+        if (self.cicle > len(self.data)):
+            return StopIteration()
+        self.cicle += 1
+        return self.data(self.cicle)
---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,15 @@
+from collections.abc import Iterable
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path: str):
+        for product in self.importer.import_data(path):
+            self.data.extends(product)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
