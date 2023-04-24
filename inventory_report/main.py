5bc65c38413acbb0793fa040ecc4c73f21475b4e
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-24 17:26:10 -0400

    func main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+import sys
+
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if (len(sys.argv) >= 3):
+        _, path, type = sys.argv
+        if path.endswith(".json"):
+            extention = JsonImporter
+        elif path.endswith(".csv"):
+            extention = CsvImporter
+        else:
+            extention = XmlImporter
+
+        response = InventoryRefactor(extention).import_data(path, type)
+        sys.stdout.write(response)
+    else:
+        sys.stderr.write("Verifique os argumentos\n")
